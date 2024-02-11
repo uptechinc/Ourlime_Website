5602d12eccb3a4b33793a18de72443c3300988fc
Raushawn <raushawnmitchell@gmail.com>
2024-02-11 01:20:30 -0400

    initial profile commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import profilePicture from '@/public/images/profilePicture.webp'
+
+export default function profile() {
+    return(
+    <div className='profile-main-container'>
+        <section className="profile-top-section">
+            <div className="">
+                <div className="profile-image-container">
+                    <Image src = {profilePicture} alt = "profile picture"/>
+                </div>
+                <div className="profile-info">
+                    <p>Darren Kowlessar</p>
+                    <p>@dareen07</p>
+                    <button>Edit</button>
+                    <button>Activities</button>
+                    <p>11</p>
+                    <p>16</p>
+                    <p>6</p>
+                    <p>Following</p>
+                    <p>Posts </p>
+                    <p>Followers</p>
+
+
+
+                </div>
+            </div>
+        </section>
+    </div>
+    )
+}
