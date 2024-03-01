9169bae7072b31fa0c07cbc0c9bf2c4287a5bb6e
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-02-29 17:09:28 -0800

    Navbar v1

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,11 @@
 import '@/styles/components/admin/styles.css'
 import Header from '@/components/admin/Header' 
+import Navbar from '@/components/admin/Navbar' 
 export default function AdminPage() {
   return (
       <div>
         <Header />
+        <Navbar />
       </div>
   )
 }
