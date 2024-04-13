d990bb1b6d6eb79f9c45cd1e4e9898a22255645e
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-04-12 20:41:45 -0400

    Chat & Video, Social Login, AI Settings Layout

--- components/admin/settings/AI.tsx
+++ components/admin/settings/AI.tsx
@@ -1,8 +1,53 @@
+import SwitchButton from "../SwitchButton";
 
 export default function AI() {
     return (
-      <div>
-        AI Content
+      <div className="ai-settings main-layout">
+        <div className="left">
+          <div className="other-config button-layout">
+            <h3>Open AI Settings</h3>
+
+            <div className="wrapper input-data">
+                <p className='sub-heading'>OpenAI API Key</p>
+                <input type="text" placeholder="Application ID"/>
+            </div>
+
+            <hr />
+
+            <div className="wrapper input-data">
+                <p className='sub-heading'>OpenAI text model</p>
+                <select name="" id="">
+                  <option value="">gpt-3.5-turbo</option>
+                  <option value="">gpt-4</option>
+                </select>
+            </div>
+
+          
+          </div>
+
+          <div className="other-config button-layout">
+            <h3>AI Settings</h3>
+
+            <div className="wrapper">
+              <div className="top">
+                <span>AI Images System</span>
+                <SwitchButton />
+              </div>
+              <p className="bottom">Allow AI to generate images.</p>
+            </div>
+
+            <hr />
+            <div className="wrapper">
+              <div className="top">
+                <span>AI Posts System</span>
+                <SwitchButton />
+              </div>
+              <p className="bottom">Allow AI to generate posts.</p>
+            </div>
+          
+          </div>
+        </div>
+
       </div>
     )
   }
--- components/admin/settings/SocialLogin.tsx
+++ components/admin/settings/SocialLogin.tsx
@@ -1,8 +1,73 @@
+import SwitchButton from "../SwitchButton";
 
 export default function SocialLogin() {
     return (
-      <div>
-        Social Login Content
+      <div className="social-settings main-layout">
+        <div className="left">
+          <div className="other-config button-layout">
+            <h3>Upload & File Sharing Configuration</h3>
+
+            <div className="wrapper">
+              <div className="top">
+                <span>Facebook Login</span>
+                <SwitchButton />
+              </div>
+              <p className="bottom">Enable the ability for users to login to your site using their Facebook account.</p>
+            </div>
+            <br />
+
+            <hr />
+            <br />
+
+            <div className="wrapper">
+              <div className="top">
+                <span>Google Login</span>
+                <SwitchButton />
+              </div>
+              <p className="bottom">Enable the ability for users to login to your site using their Google+ account, (App requires reviewing)</p>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="right">
+          <div className="general-config button-layout">
+            <h3>Setup Social Login API Keys</h3>
+            <div className="wrapper input-data">
+                <p className='sub-heading' 
+                  style={{
+                    backgroundColor: '#1877f2', 
+                    color: 'white',
+                    borderRadius: '1rem',
+                    padding: '1rem',
+                    width:  'fit-content'
+                    }}>Facebook Configuration</p>
+                  <input type="text" placeholder="Application ID"/>
+                  <br />
+                  <input type="text" placeholder="Application Secret Key"/>
+            </div>
+            <br />
+
+            <hr />
+
+            <div className="wrapper input-data">
+                <p className='sub-heading'
+                style={{
+                  backgroundColor: '#dd5144', 
+                  color: 'white',
+                  borderRadius: '1rem',
+                  padding: '1rem',
+                  width:  'fit-content'
+                  }}>Google Configuration</p>
+                  <input type="text" placeholder="Client ID"/>
+                  <br />
+                  <input type="text" placeholder="Client Secret Key"/>
+            </div>
+
+          </div>
+
+        </div>
       </div>
     )
   }
