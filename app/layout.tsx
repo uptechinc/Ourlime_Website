35400d961b2085eb0bcbc3e6308dc81fde092ab7
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-06-14 13:59:31 -0400

    no idea

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: ReactElement;
 }): ReactElement {
   return (
-      <html lang="en">
+    <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -46,18 +46,36 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary_large_image" />
       </head>
 
-      <body className={inter.className}>
+      <body className="bg-[#f2f2f2] overflow-x-hidden font-sans p-0 m-0">
         {children}
-        
-        <div className="btn-wrapper">
-          <svg width="378" height="362" viewBox="0 0 378 362" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="199" y1="-6" x2="199" y2="362" stroke="white" strokeWidth="20"/>
-            <line x1="378" y1="188" x2="-8" y2="188" stroke="white" strokeWidth="20"/>
+
+        <div className="flex justify-center items-center gap-4 bg-[#c64d53] fixed w-3 h-3 bottom-[5%] right-[5%] p-1 rounded-full cursor-pointer">
+          <svg
+            width="378"
+            height="362"
+            viewBox="0 0 378 362"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="199"
+              y1="-6"
+              x2="199"
+              y2="362"
+              stroke="white"
+              strokeWidth="20"
+            />
+            <line
+              x1="378"
+              y1="188"
+              x2="-8"
+              y2="188"
+              stroke="white"
+              strokeWidth="20"
+            />
           </svg>
         </div>
-
-
-        </body>
-      </html>
+      </body>
+    </html>
   );
 }
